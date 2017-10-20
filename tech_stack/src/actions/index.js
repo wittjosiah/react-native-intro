b8e55eba32477624c0bc0055acb3ef21638444cd
Josiah Witt <josiah@witt.life>
2017-10-19 21:38:02 -0400

    Finished tech_stack app

---
+++ tech_stack/src/actions/index.js
@@ -0,0 +1,7 @@
+export const selectLibrary = libraryId => {
+  console.log(libraryId);
+  return {
+    type: 'select_library',
+    payload: libraryId
+  };
+};
---
+++ tech_stack/src/components/ListItem.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation
+} from 'react-native';
+import { connect } from 'react-redux';
+import { CardSection } from './common';
+import * as actions from '../actions';
+
+class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+  
+  renderDescription() {
+    const { selected, library } = this.props;
+
+    if (selected) {
+      return (
+        <CardSection>
+          <Text>{library.description}</Text>
+        </CardSection>
+      );
+    }
+  }
+
+  render() {
+    const { titleStyle } = styles;
+    const { id, title, selected } = this.props.library;
+    console.log(this.props);
+
+    return(
+      <TouchableWithoutFeedback
+        onPress={() => this.props.selectLibrary(id)}
+      >
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>
+              {title}
+            </Text>
+          </CardSection>
+          {this.renderDescription()}
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+};
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    selected: ownProps.library.id === state.selectedLibraryId
+  };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
